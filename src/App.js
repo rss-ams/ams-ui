6b649fbfb0baab01c1ed5ab0bb51490f722cc9c2
Gautam Kumar <gautamguptabasant@gmail.com>
2020-11-27 01:07:28 +0530

    Added drawer for naigation and fixed layout for Fields page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import ApplnBar from "./components/ApplnBar";
-import SideMenu from "./components/SideMenu";
 
 function App() {
   return (
     <div className="App">
       <ApplnBar />
-      <SideMenu />
     </div>
   );
 }
