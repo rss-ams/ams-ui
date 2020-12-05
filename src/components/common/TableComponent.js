ba3d04835ac8696d18974440ba365b7c0fae2467
Aashna Prakash <aashna.prakash@sap.com>
2020-12-05 13:49:11 +0530

    fix(ams-ui): review comments fix

--- src/components/common/TableComponent.js
+++ src/components/common/TableComponent.js
@@ -12,31 +12,29 @@ import Tooltip from '@material-ui/core/Tooltip';
 import DeleteIcon from '@material-ui/icons/Delete';
 import SvgIcon from '@material-ui/core/SvgIcon';
 
-
 /**
  * css styles for table
  */
 const useStyles = makeStyles({
   root: {
     margin: '20px 0 20px 0',
-    width: '100%'
-    
+    width: '100%',
   },
   container: {
-    maxHeight: 400
+    maxHeight: 400,
   },
   noDataContainer: {
-    textAlign: 'center'
+    textAlign: 'center',
   },
   noDataSpan: {
     fontWeight: 'normal',
     verticalAlign: 'middle',
-    fontSize: '14px'
+    fontSize: '14px',
   },
   iconBut: {
     padding: 0,
-    borderRadius: 0
-  }
+    borderRadius: 0,
+  },
 });
 
 /**
@@ -46,14 +44,19 @@ const useStyles = makeStyles({
  * deletehandler will be called when user clicks on delete button for a single row
  * edithandler will be called when user clicks on edit  button for a single row
  * A single column can have properties like id, label, align, minWidth, type
- * @param {*}  
+ * @param {*}
  */
-export default function TableComponent({cols, rows, deleteHandler, editHandler}) {
+export default function TableComponent({
+  cols,
+  rows,
+  deleteHandler,
+  editHandler,
+}) {
   const classes = useStyles();
   return (
     <Paper className={classes.root}>
       <TableContainer className={classes.container}>
-        <Table stickyHeader aria-label="Data" size="small">
+        <Table stickyHeader aria-label='Data' size='small'>
           <TableHead>
             <TableRow>
               {cols.map((column) => (
@@ -70,28 +73,51 @@ export default function TableComponent({cols, rows, deleteHandler, editHandler})
           <TableBody>
             {rows.map((row) => {
               return (
-                <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                <TableRow hover role='checkbox' tabIndex={-1} key={row.code}>
                   {cols.map((column) => {
                     const value = row[column.id];
                     return (
                       <TableCell key={column.id} align={column.align}>
-                        {column.format && typeof value === 'number' ? column.format(value) : value}
-                        {column.type === 'icon'?
-                        (<Tooltip title={column.id}>
-                          {column.id === 'delete'?
-                          (<IconButton className={classes.iconBut}  aria-label={column.id} onClick={() => deleteHandler(row)}><DeleteIcon /></IconButton>):
-                          (<IconButton className={classes.iconBut}  aria-label={column.id} onClick={() => editHandler(row)}><SvgIcon><path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34a.9959.9959 0 00-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"></path></SvgIcon></IconButton>)}
-                        </Tooltip>): null}
+                        {column.format && typeof value === 'number'
+                          ? column.format(value)
+                          : value}
+                        {column.type === 'icon' ? (
+                          <Tooltip title={column.id}>
+                            {column.id === 'delete' ? (
+                              <IconButton
+                                className={classes.iconBut}
+                                aria-label={column.id}
+                                onClick={() => deleteHandler(row)}
+                              >
+                                <DeleteIcon />
+                              </IconButton>
+                            ) : (
+                              <IconButton
+                                className={classes.iconBut}
+                                aria-label={column.id}
+                                onClick={() => editHandler(row)}
+                              >
+                                <SvgIcon>
+                                  <path d='M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34a.9959.9959 0 00-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z'></path>
+                                </SvgIcon>
+                              </IconButton>
+                            )}
+                          </Tooltip>
+                        ) : null}
                       </TableCell>
                     );
                   })}
                 </TableRow>
-              );})}
+              );
+            })}
           </TableBody>
         </Table>
       </TableContainer>
-      {!rows.length?(<div className={classes.noDataContainer}><span className={classes.noDataSpan}>No Data</span></div>): null}
+      {!rows.length ? (
+        <div className={classes.noDataContainer}>
+          <span className={classes.noDataSpan}>No Data</span>
+        </div>
+      ) : null}
     </Paper>
-    
   );
 }
