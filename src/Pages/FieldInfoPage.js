648bf4e032d80fe91a3a8d7518d5e72ea20d9b21
Mahima Vuppuluri <vuppulur@Mahimas-MacBook-Pro.local>
2020-12-11 15:29:14 +0530

    removing unused imports

--- src/Pages/FieldInfoPage.js
+++ src/Pages/FieldInfoPage.js
@@ -7,7 +7,6 @@ import {
   Select,
   Snackbar,
   Typography,
-  TextField,
 } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { Alert } from '@material-ui/lab';
